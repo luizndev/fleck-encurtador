f88e62bae54b612efb6a02a90eeadf40b1b2dbc8
Luis Eduardo Andrade <79378948+luizndev@users.noreply.github.com>
2025-06-05 19:50:38 -0300

    Commit CSS
    Adicionado um pouco de responsividade para tamanhos de tela mobiles!

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,13 +1,19 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { Link } from "react-router-dom"
 import Logotipo from "../../assets/logotipo.svg"
 import { PiGithubLogo } from "react-icons/pi";
 import { IoMdBook } from "react-icons/io";
+import { RiMenuFill } from "react-icons/ri";
 import { CiLinkedin } from "react-icons/ci";
 
 import "./Header.css"
 
 const Header = () => {
+  const [ menu, setMenu ] = useState(false)
+
+  useEffect(() => {
+    console.log(menu)
+  }, [menu])
   const handleHome = () => {
       window.location.href = "/"
   }
@@ -18,25 +24,53 @@ const Header = () => {
        </div>
 
        <ul className="more-links">
-        <li>
+        <li className='menu-burger-li' onClick={() => setMenu(!menu)}>
+            {menu}
+            <RiMenuFill className='menu-burger' />
+        </li>
+        <li className='li-option'>
             <Link to="https://github.com/luizndev">
                 <PiGithubLogo />
                 GitHub
             </Link>
          </li>
-        <li>
+        <li className='li-option'>
             <Link to="https://www.linkedin.com/in/luiseduardo-andrade/">
                 <CiLinkedin />
                 Linkedin
             </Link>
          </li>
-        <li>
+        <li className='li-option'>
             <Link to="https://luizndev.vercel.app/">
                 <IoMdBook />
                 Portifolio
             </Link>
          </li>
        </ul>
+
+       {menu ? (
+        <ul className='mobile-menu'>
+            <li>
+                <Link to="https://github.com/luizndev">
+                    <PiGithubLogo />
+                    GitHub
+                </Link>
+            </li>
+            <li>
+                <Link to="https://www.linkedin.com/in/luiseduardo-andrade/">
+                    <CiLinkedin />
+                    Linkedin
+                </Link>
+            </li>
+            <li>
+                <Link to="https://luizndev.vercel.app/">
+                    <IoMdBook />
+                    Portifolio
+                </Link>
+            </li>
+        </ul>
+       ) : ""}
+
     </header>
   )
 }
